69325b2ecef073df5f6b35142e73f1843c68dbcc
whitelon <happysensey@gmail.com>
2018-11-30 23:02:08 +0300

    singled out file searching

--- server.py
+++ server.py
@@ -17,32 +17,34 @@ async def upload(request):
         raise web.HTTPBadRequest(text="'file' key was not found")
 
 
-@routes.get('/{hash}')
-async def download(request):
+def find_file(request, action):
     file_hash = request.match_info['hash']
 
     if not storage.valid_hash(file_hash):
         raise web.HTTPBadRequest(text='invalid hash parameter')
 
     try:
-        file = storage.get_path(file_hash)
-        return web.FileResponse(file)
+        return action(file_hash)
     except FileNotFoundError:
         raise web.HTTPNotFound()
+
+
+@routes.get('/{hash}')
+async def download(request):
+    def download_action(file_hash):
+        file = storage.get_path(file_hash)
+        return web.FileResponse(file)
+
+    return find_file(request, download_action)
 
 
 @routes.delete('/{hash}')
 async def delete(request):
-    file_hash = request.match_info['hash']
-
-    if not storage.valid_hash(file_hash):
-        raise web.HTTPBadRequest(text='invalid hash parameter')
-
-    try:
+    def delete_action(file_hash):
         storage.delete(file_hash)
         return web.Response(text='File deleted')
-    except FileNotFoundError:
-        raise web.HTTPNotFound()
+
+    return find_file(request, delete_action)
 
 
 app = web.Application()
